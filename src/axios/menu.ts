614e72d6344fc13675092dcd92a99d507b85bca7
knowave <kwaktaemin98@gmail.com>
2024-06-15 13:29:10 +0900

    수정

--- src/axios/menu.ts
+++ src/axios/menu.ts
@@ -8,12 +8,13 @@ export const fetchMenus = async (
   try {
     const response = await axios.get("http://localhost:8080/menu");
     const fetchedMenus: Menu[] = response.data;
-
+    
     if (fetchedMenus.length === 0) {
       setMenus(mockMenus);
     } else {
       setMenus(fetchedMenus);
     }
+
   } catch (error) {
     console.error("Error fetching menus:", error);
     setMenus(mockMenus);
