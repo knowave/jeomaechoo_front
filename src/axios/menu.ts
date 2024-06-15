7a49839d6ed43030e7bc1305f0e2700458ce7744
knowave <kwaktaemin98@gmail.com>
2024-06-15 16:20:05 +0900

    import 수정

--- src/axios/menu.ts
+++ src/axios/menu.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Menu } from "../interfaces/menu.interface";
 import { mockMenus } from "../mock/menu.mock";
-import { SERVER_URL } from "../../";
+import { SERVER_URL } from "../../env";
 
 export const fetchMenus = async (
   setMenus: React.Dispatch<React.SetStateAction<Menu[]>>
@@ -9,13 +9,12 @@ export const fetchMenus = async (
   try {
     const response = await axios.get(`${SERVER_URL}/menu`);
     const fetchedMenus: Menu[] = response.data;
-    
+
     if (fetchedMenus.length === 0) {
       setMenus(mockMenus);
     } else {
       setMenus(fetchedMenus);
     }
-
   } catch (error) {
     console.error("Error fetching menus:", error);
     setMenus(mockMenus);
